622fd7356d41185697460b5b412c245ebf5371b6
nalendro16 <nalendro_agung@yahoo.com>
2021-03-17 07:15:11 +0700

    make navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,11 @@ const Navbar = () => {
   return (
     <nav>
       <h1>Breaking Context</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
     </nav>
   )
 }
